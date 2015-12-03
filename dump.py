c4801be1ee6b5259468b4dd3c24a7b0a8cd0cc8c
Pavel Holica <pholica@redhat.com>
2015-12-03 11:02:50 +0100

    Increase dump size.

--- dump.py
+++ dump.py
@@ -3,7 +3,8 @@
 import os
 os.environ["DISPLAY"] = ":1"
 import dogtail.utils
+import dogtail.config
 dogtail.utils.enableA11y()
-from dogtail.predicate import GenericPredicate
+dogtail.config.config.childrenLimit = 10000
 import dogtail.tree
 dogtail.tree.root.dump(fileName='/tmp/dogtail.dump')
