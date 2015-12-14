056de6e2f1958a3cd7f7a530d197ec45d8f3a8e0
Pavel Holica <pholica@redhat.com>
2015-12-14 10:58:35 +0100

    Install dogtail to different location

--- anabot-prepare.sh
+++ anabot-prepare.sh
@@ -9,11 +9,11 @@ sleep 5
 pushd /opt/dogtail
 tar xzf /opt/dogtail/dogtail-0.9.0.tar.gz
 pushd dogtail-0.9.0
-python setup.py install
+python setup.py install --prefix /opt/
 popd
 
-sed -i 's/os\.getlogin()/"root"/g' /usr/lib/python2.7/site-packages/dogtail/config.py
-sed -i 's/os\.environ\['"'USER'"'\]/"root"/g' /usr/lib/python2.7/site-packages/dogtail/config.py
+sed -i 's/os\.getlogin()/"root"/g' /opt/lib/python2.7/site-packages/dogtail/config.py
+sed -i 's/os\.environ\['"'USER'"'\]/"root"/g' /opt/lib/python2.7/site-packages/dogtail/config.py
 popd
 
 mkdir -p /var/run/anabot 2> /dev/null
