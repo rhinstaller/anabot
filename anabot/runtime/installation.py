041ca4a3a6e038cb9134b8fa858488c49c9872fc
Pavel Holica <pholica@redhat.com>
2016-01-06 11:38:46 +0100

    Added translations, anabot should now run in any language.

--- anabot/runtime/installation.py
+++ anabot/runtime/installation.py
@@ -57,13 +57,14 @@ def welcome_locality_check(element, app_node, local_node):
 @handle_action('/installation/hub')
 def hub_handler(element, app_node, local_node):
     default_handler(element, app_node, local_node)
-    begin_button = getnode(app_node, "push button", "Begin Installation")
+    begin_button = getnode(app_node, "push button",
+                           tr("_Begin Installation", context="GUI|Summary"))
     begin_button.click()
 
 @handle_action('/installation/hub/partitioning')
 def hub_partitioning_handler(element, app_node, local_node):
     partitioning = getnode(app_node, "spoke selector",
-                           "INSTALLATION DESTINATION")
+                           tr("INSTALLATION DESTINATION"))
     partitioning.click()
     default_handler(element, app_node, local_node)
 
@@ -77,7 +78,7 @@ def hub_partitioning_handler_disk(element, app_node, local_node):
     for disk in disks:
         # selected disk has icon without name
         icon = getnode(disk, node_type="icon")
-        if action == "select" and icon.name == "Hard Disk":
+        if action == "select" and not disk.name != "":
             disk.click()
         elif action == "deselect" and icon.name == "":
             disk.click()
@@ -88,9 +89,9 @@ def hub_partitioning_handler_mode(element, app_node, local_node):
     if mode == "default":
         return
     if mode == "automatic":
-        radio_text = "Automatically configure partitioning."
+        radio_text = tr("A_utomatically configure partitioning.")
     if mode == "manual":
-        radio_text = "I will configure partitioning."
+        radio_text = tr("_I will configure partitioning.")
     radio = getnode(app_node, "radio button", radio_text)
     if not radio.checked:
         radio.click()
@@ -98,43 +99,48 @@ def hub_partitioning_handler_mode(element, app_node, local_node):
 @handle_action('/installation/hub/partitioning/additional_space')
 def hub_partitioning_handler_additional_space(element, app_node, local_node):
     action = get_attr(element, "action", "enable")
-    checkbox_text = "I would like to make additional space available."
+    checkbox_text = tr("I would like to _make additional space available.")
     additional_checkbox = getnode(app_node, "check box", checkbox_text)
     if (action == "enable") != additional_checkbox.checked:
         additional_checkbox.click()
 
 @handle_action('/installation/hub/partitioning/done')
 def hub_partitioning_handler_done(element, app_node, local_node):
-    destination_panel = getnode(app_node, "panel", "INSTALLATION DESTINATION")
-    done_button = getnode(destination_panel, "push button", "_Done")
+    destination_panel = getnode(app_node, "panel",
+                                tr("INSTALLATION DESTINATION"))
+    done_button = getnode(destination_panel, "push button", tr("_Done", False))
     done_button.click()
 
 @handle_action('/installation/hub/partitioning/reclaim')
 def hub_partitioning_handler_reclaim(element, app_node, local_node):
     # TODO action=reclaim/cancel
     default_handler(element, app_node, local_node)
-    reclaim_button = getnode(app_node, "push button", "Reclaim space")
+    reclaim_button = getnode(app_node, "push button",
+                             tr("_Reclaim space", context="GUI|Reclaim Dialog"))
     reclaim_button.click()
 
 @handle_action('/installation/hub/partitioning/reclaim/delete_all')
 def hub_partitioning_handler_reclaim_delete_all(element, app_node, local_node):
-    delete_all_button = getnode(app_node, "push button", "Delete all")
+    delete_all_button = getnode(app_node, "push button",
+                                tr("Delete _all", context="GUI|Reclaim Dialog"))
     delete_all_button.click()
 
 @handle_action('/installation/configuration')
 def hub_configuration_handler(element, app_node, local_node):
     default_handler(element, app_node, local_node)
     logger.debug("WAITING FOR REBOOT")
-    reboot_button = getnode(app_node, "push button", "Reboot",
+    reboot_button = getnode(app_node, "push button",
+                            tr("_Reboot", context="GUI|Progress"),
                             timeout=float("inf"))
     reboot_button.click()
 
 @handle_action('/installation/configuration/root_password')
 def configuration_root_password_handler(element, app_node, local_node):
-    root_password_spoke = getnode(app_node, "spoke selector", "ROOT PASSWORD")
+    root_password_spoke = getnode(app_node, "spoke selector",
+                                  tr("_ROOT PASSWORD", context="GUI|Spoke"))
     root_password_spoke.click()
     try:
-        root_password_panel = getnode(app_node, "panel", "ROOT PASSWORD")
+        root_password_panel = getnode(app_node, "panel", tr("ROOT PASSWORD"))
     except TimeoutError:
         return (False, "Root password spoke not found")
     default_handler(element, app_node, root_password_panel)
@@ -142,21 +148,23 @@ def configuration_root_password_handler(element, app_node, local_node):
 @handle_action('/installation/configuration/root_password/password')
 def configuration_root_password_text_handler(element, app_node, local_node):
     value = get_attr(element, "value")
-    password_entry = getnode(local_node, "password text", "Password")
+    password_entry = getnode(local_node, "password text", tr("Password"))
     password_entry.click()
     password_entry.typeText(value)
 
 @handle_action('/installation/configuration/root_password/confirm_password')
 def configuration_root_password_confirm_handler(element, app_node, local_node):
     value = get_attr(element, "value")
-    password_entry = getnode(local_node, "password text", "Confirm Password")
+    password_entry = getnode(local_node, "password text",
+                             tr("Confirm Password"))
     password_entry.click()
     password_entry.typeText(value)
 
 @handle_action('/installation/configuration/root_password/done')
 def configuration_root_password_done_handler(element, app_node, local_node):
     try:
-        root_password_done = getnode(local_node, "push button", "_Done")
+        root_password_done = getnode(local_node, "push button",
+                                     tr("_Done", False))
     except TimeoutError:
         return (False, "Done button not found or not clickable")
 
