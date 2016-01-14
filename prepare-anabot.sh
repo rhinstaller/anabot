47aede6d72acb4ce0e6935c5bad568ce76702e35
Pavel Holica <pholica@redhat.com>
2016-01-14 17:39:39 +0100

    Include custom fix for dogtail bug.
    https://fedorahosted.org/dogtail/ticket/64

--- prepare-anabot.sh
+++ prepare-anabot.sh
@@ -7,8 +7,8 @@ done
 sleep 5
 
 pushd /opt/dogtail
-tar xzf /opt/dogtail/dogtail-0.9.0.tar.gz
-pushd dogtail-0.9.0
+tar xzf /opt/dogtail/dogtail-0.9.1.tar.gz
+pushd dogtail-0.9.1
 python setup.py install --prefix /opt/
 popd
 
