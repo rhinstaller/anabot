f5cfd0f8eb5362a953053ab00bff08e86044d40b
Petr Janda <pjanda@redhat.com>
2016-01-15 15:43:23 +0100

    Add own /etc config for initial setup
     place systemd units for initial setup testing to /etc

--- make_updates-is.sh
+++ make_updates-is.sh
@@ -4,7 +4,7 @@ DESTINATION=${1:-../updates-is.tar.gz}
 TMPDIR=`mktemp -d`
 mkdir $TMPDIR/opt
 cp -r * $TMPDIR/opt
-# mv $TMPDIR/opt/etc $TMPDIR
+mv $TMPDIR/opt/etc-is $TMPDIR/etc
 
 tar -C $TMPDIR -czf $DESTINATION .
 
