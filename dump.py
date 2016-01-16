c1aff927de84cd81db6913bb615b5f78259e7d32
Petr Janda <pjanda@redhat.com>
2016-01-16 11:36:33 +0100

    dump.py now can use different DISPLAY
      if DISPLAY environment variable is set dump.py uses it

--- dump.py
+++ dump.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2
 
 import os
-os.environ["DISPLAY"] = ":1"
 import sys
 sys.path.append('/opt/lib/python2.7/site-packages')
+
+if "DISPLAY" not in os.environ:
+    os.environ["DISPLAY"] = ":1"
+print 'using DISPLAY="%s"' % os.environ['DISPLAY']
+
 import dogtail.utils
 import dogtail.config
 dogtail.utils.enableA11y()
