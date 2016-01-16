354266084da5b380041b6a30c7d214f9be230520
Petr Janda <pjanda@redhat.com>
2016-01-16 11:43:29 +0100

    anabot can handle license spoke now
      with following xml anabot can accept license
       it still does not finish configuration
    
      <initial_setup>
      <hub>
        <license>
          <accept_license checked="yes"/>
          <done/>
        </license>
      </hub>
      </initial_setup>

--- anabot/runtime/initial_setup/hub/__init__.py
+++ anabot/runtime/initial_setup/hub/__init__.py
@@ -10,6 +10,12 @@ from anabot.runtime.translate import tr
 
 from dogtail.predicate import GenericPredicate
 
+# submodules
+from . import license
+#, subscription_manager
+
 @handle_action('/initial_setup/hub')
 def hub_handler(element, app_node, local_node):
     default_handler(element, app_node, local_node)
+    # ToDo
+    # find FINISH CONFIGURATION button and click it
