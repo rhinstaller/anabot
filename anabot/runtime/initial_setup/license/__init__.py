0acbdc63fa2583765887188870fcf4aeeeb4f80e
Petr Janda <pjanda@redhat.com>
2016-01-21 14:42:25 +0100

    Add check for EULA text
     displayed EULA is compared against file from redhat-release pkg

--- anabot/runtime/initial_setup/license/__init__.py
+++ anabot/runtime/initial_setup/license/__init__.py
@@ -28,14 +28,32 @@ def empty_handler(element, app_node, local_node):
 
 @handle_chck('/eula')
 def eula_chck(element, app_node, local_node):
-    # license file location ./usr/share/redhat-release/EULA
+    def reformat_eula(file_name):
+        eula_file = file(file_name)
+        # reformat stored eula according to initial setup
+        fobj_lines = eula_file.xreadlines()
+        stored_eula = ""
+        firstline = True
+        for line in fobj_lines:
+            stripped_line = line.strip()
+            if stripped_line:
+                if firstline:
+                    stored_eula = stripped_line
+                    firstline = False
+                else:
+                    stored_eula += " " + stripped_line
+            else:
+                stored_eula += "\n\n"
+        eula_file.close()
+        return stored_eula
+
+    license_file = "/usr/share/redhat-release/EULA"
     license_label = getnode(app_node, "label", tr("License Agreement:"))
     license_panel = license_label.parent.parent
     license_text = getnode(license_panel, 'text', '')
     displayed_eula = license_text.text
-    # ToDo load and check eula text
-    # stored_eula = #ToDo
-    return (False, 'Not implemented yet')
+    return displayed_eula == reformat_eula(license_file)
+    
 
 @handle_act('/accept_license')
 def accept_license_handler(element, app_node, local_node):
