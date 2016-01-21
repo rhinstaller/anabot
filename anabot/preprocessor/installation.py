d1844fe4c0ae8d0009af0f3443e3d84ddee58c11
Pavel Holica <pholica@redhat.com>
2016-01-21 12:21:42 +0100

    preprocessor/installation: New approach for defaults.
    Defaults are now not directly in snippets, but rather small snippet is loaded
    and root element is handled as "easy", then replace is directly called on it.

--- anabot/preprocessor/installation.py
+++ anabot/preprocessor/installation.py
@@ -5,7 +5,7 @@ logger = logging.getLogger('anabot.preprocessor')
 import re
 
 from .decorators import replace, default
-from .functions import load_snippet, has_property, pop_property
+from .functions import load_snippet, has_property, pop_property, copy_content
 from .defaults import delete_element
 
 @replace("/installation")
@@ -15,35 +15,55 @@ def replace_installation(element):
         first_el = element.xpathEval("./*")[0]
         if first_el != new_welcome:
             first_el.addPrevSibling(new_welcome)
+        replace_welcome(new_welcome, element)
     if len(element.xpathEval('./hub')) == 0:
         new_hub = element.addChild(load_snippet("/installation/hub", element, tag_name='_default_for'))
         welcome_el = element.xpathEval("./*")[0]
         welcome_el.addNextSibling(new_hub)
+        replace_hub(new_hub, element)
     if len(element.xpathEval('./configuration')) == 0:
         new_conf = element.addChild(load_snippet("/installation/configuration", element, tag_name='_default_for'))
+        replace_configuration(new_conf, element)
 
     return element
 
 @replace("/installation/welcome")
-def replace_welcome(element):
+def replace_welcome(element, default_for=None):
+    if default_for is None:
+        default_for = element
     lang_re = re.compile(r"(?P<lang>[^(]*) (?:\((?P<loc>[^)]*)\))?")
     lang_prop = pop_property(element, "language")
     if lang_prop is not None:
         new = load_snippet("/installation/welcome@language", element, True)
-        element.replaceNode(new)
+        copy_content(new, element)
         match = lang_re.match(lang_prop.content)
         lang = match.group("lang")
-        new.xpathEval("./language")[0].setProp("value", lang)
+        element.xpathEval("./language")[0].setProp("value", lang)
         loc = match.group("loc")
-        new.xpathEval("./locality")[0].setProp("value", loc)
-        return new
+        element.xpathEval("./locality")[0].setProp("value", loc)
     return element
 
+@replace("/installation/hub")
+def replace_hub(element, default_for=None):
+    if default_for is None:
+        default_for = element
+    if len(element.xpathEval('./partitioning')) == 0:
+        new = load_snippet("/installation/hub/autopart", default_for, tag_name="_default_for")
+        element.addChild(new)
+
 @replace("/installation/hub/autopart")
-def replace_autopart(element):
+def replace_autopart(element, default_for=None):
     new = load_snippet("/installation/hub/autopart", element)
     element.replaceNode(new)
     return new
+
+@replace("/installation/configuration")
+def replace_configuration(element, default_for=None):
+    if default_for is None:
+        default_for = element
+    if len(element.xpathEval("./root_password")) == 0:
+        new = load_snippet("/installation/configuration/root", default_for, tag_name="_default_for")
+        element.addChild(new)
 
 @replace("/installation/configuration/root")
 def replace_rootpw(element):
