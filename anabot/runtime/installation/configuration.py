4b4a9100f746de3f47539bcdcb10b5a43cf3e178
Petr Janda <pjanda@redhat.com>
2016-02-13 13:35:23 +0100

    Add code for user spoke
      It can now create user during installation

--- anabot/runtime/installation/configuration.py
+++ anabot/runtime/installation/configuration.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import logging
 logger = logging.getLogger('anabot')
 
 from anabot.runtime.decorators import handle_action, handle_check
 from anabot.runtime.default import default_handler
-from anabot.runtime.functions import get_attr, getnode, TimeoutError
+from anabot.runtime.functions import get_attr, getnode, TimeoutError, getparent, getsibling
 from anabot.runtime.translate import tr
 
 _local_path = '/installation/configuration'
@@ -55,3 +56,228 @@ def root_password_done_handler(element, app_node, local_node):
 
     root_password_done.click()
     return True # done for password found and was clicked
+
+@handle_act('/create_user')
+def user_spoke_handler(element, app_node, local_node):
+    user_spoke = getnode(app_node, "spoke selector",
+                                  tr("_USER CREATION", context="GUI|Spoke"))
+    user_spoke.click()
+    try:
+        user_panel = getnode(app_node, "panel", tr("CREATE USER"))
+    except TimeoutError:
+        return (False, "User creation spoke not found")
+    default_handler(element, app_node, user_panel)
+
+@handle_act('/create_user/full_name')
+def user_full_name_handler(element, app_node, local_node):
+    entry = getnode(local_node, 'text', tr('Full Name'))
+    value = get_attr(element, 'value')
+    entry.typeText(value)
+
+@handle_chck('/create_user/full_name')
+def user_full_name_check(element, app_node, local_node):
+    entry = getnode(local_node, 'text', tr('Full Name'))
+    value = get_attr(element, 'value')
+    if (value is None):
+        value = ''
+    if entry.text == value:
+        return True
+    return (False, "users full name ('%s') differs from expected one ('%s')" % (entry.text, value))
+
+@handle_act('/create_user/username')
+def user_username_handler(element, app_node, local_node):
+    entry = getnode(local_node, 'text', tr('User name'))
+    value = get_attr(element, 'value')
+    if not (value is None):
+        entry.typeText(value)
+
+@handle_chck('/create_user/username')
+def user_username_check(element, app_node, local_node):
+    entry = getnode(local_node, 'text', tr('User name'))
+    value = get_attr(element, 'value')
+    if (value is None):
+        value = ''
+    if entry.text == value:
+        return True
+    return (False, "username ('%s') differs from expected one ('%s')" % (entry.text, value))
+
+@handle_act('/create_user/is_admin')
+def user_is_admin_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Make this user administrator', context="GUI|User")) # translation bug: "Udělat tohot uživatele správcem"
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked != value:
+        checkbox.click()
+
+@handle_chck('/create_user/is_admin')
+def user_is_admin_check(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Make this user administrator', context="GUI|User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked == value:
+        return True
+    return False
+        
+
+@handle_act('/create_user/require_password')
+def user_is_admin_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Require a password to use this account', context="GUI|User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked != value:
+        checkbox.click()
+
+@handle_chck('/create_user/require_password')
+def user_is_admin_check(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Require a password to use this account', context="GUI|User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked == value:
+        return True
+    return False
+
+@handle_act('/create_user/password')
+def user_password_handler(element, app_node, local_node):
+    entry = getnode(local_node, "password text", tr('_Password', context="GUI|User"))
+    password = get_attr(element, 'value')
+    entry.typeText(password)
+    return True
+
+@handle_act('/create_user/confirm_password')
+def user_password_handler(element, app_node, local_node):
+    print "searching for %s" % tr('Confirm password')
+    entry = getnode(local_node, "password text", tr('Confirm Password')) # translation error  label "_Povrďte heslo"
+    password = get_attr(element, 'value')
+    entry.typeText(password)
+    return True
+
+@handle_act('/create_user/advanced')
+def user_advanced_handler(element, app_node, local_node):
+    button = getnode(local_node, "push button", tr('_Advanced...', context="GUI|User"))
+    button.click()
+    dialog_label = getnode(app_node, "label", tr('ADVANCED USER CONFIGURATION'))
+    advanced_dialog = getparent(dialog_label, node_type="dialog")
+    default_handler(element, app_node, advanced_dialog)
+
+@handle_chck('/create_user/advanced')
+def user_advanced_check(element, app_node, local_node):
+    button = getnode(local_node, "push button", tr('_Advanced...', context="GUI|User"), sensitive = None)
+    return button.sensitive
+
+@handle_act('/create_user/advanced/home')
+def user_adv_homedir_handler(element, app_node, local_node):
+    label = getnode(local_node, "label", tr('Home _directory:', context="GUI|Advanced User"))
+    entry = getsibling(label, 1, node_type="text")
+    homedir = get_attr(element, 'value')
+    entry.typeText(homedir)
+
+@handle_chck('/create_user/advanced/home')
+def user_adv_homedir_check(element, app_node, local_node):
+    label = getnode(local_node, "label", tr('Home _directory:', context="GUI|Advanced User"))
+    print 'found homedir label'
+    entry = getsibling(label, 1, node_type="text")
+    homedir = get_attr(element, 'value')
+    if entry.text == homedir:
+        return True
+    return (False, "Homedir '%s' is different than expected '%s'" % (entry.text, homedir))
+
+@handle_act('/create_user/advanced/manual_uid')
+def user_adv_manual_uid_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _user ID manually:', context="GUI|Advanced User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked != value:
+        checkbox.click()
+
+@handle_chck('/create_user/advanced/manual_uid')
+def user_adv_manual_uid_check(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _user ID manually:', context="GUI|Advanced User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked == value:
+        return True
+    return False
+
+@handle_act('/create_user/advanced/uid')
+def user_adv_uid_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _user ID manually:', context="GUI|Advanced User"))
+    spin_button = getsibling(checkbox, -1, node_type="spin button", sensitive=None)
+    value = get_attr(element, 'value')
+    spin_button.typeText(value)
+
+@handle_act('/create_user/advanced/manual_gid')
+def user_adv_manual_gid_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _group ID manually:', context="GUI|Advanced User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked != value:
+        checkbox.click()
+
+@handle_chck('/create_user/advanced/manual_gid')
+def user_adv_manual_gid_check(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _group ID manually:', context="GUI|Advanced User"))
+    value = get_attr(element, 'checked')
+    if value == 'yes':
+        value = True
+    else:
+        value = False
+    if checkbox.checked == value:
+        return True
+    return False
+
+@handle_act('/create_user/advanced/gid')
+def user_adv_gid_handler(element, app_node, local_node):
+    checkbox = getnode(local_node, "check box", tr('Specify a _group ID manually:', context="GUI|Advanced User"))
+    spin_button = getsibling(checkbox, -2, node_type="spin button", sensitive=None)
+    value = get_attr(element, 'value')
+    spin_button.typeText(value)
+
+@handle_act('/create_user/advanced/groups')
+def user_adv_groups_handler(element, app_node, local_node):
+    label = getnode(local_node, "label", tr('_Add user to the following groups:', context="GUI|Advanced User"))
+    entry = getsibling(label, 1, node_type="text")
+    groups = get_attr(element, 'value')
+    entry.typeText(groups)
+
+@handle_act('/create_user/advanced/cancel')
+def user_adv_cancel_handler(element, app_node, local_node):
+    button = getnode(local_node, "push button", tr("_Cancel", context="GUI|Advanced User"))
+    button.click()
+
+@handle_act('/create_user/advanced/save')
+def user_adv_save_handler(element, app_node, local_node):
+    button = getnode(local_node, "push button", tr("_Save Changes", context="GUI|Advanced User"))
+    button.click()
+
+@handle_act('/create_user/done')
+def root_password_done_handler(element, app_node, local_node):
+    try:
+        done_btn = getnode(local_node, "push button",
+                                     tr("_Done", False))
+    except TimeoutError:
+        return (False, "Done button not found or not clickable")
+
+    done_btn.click()
+    return True # done for password found and was clicked
+
