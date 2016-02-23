576acec592506328403ac32e77ebb0e7ad711517
Pavel Holica <pholica@redhat.com>
2016-02-23 08:19:23 +0100

    tests: Fail testsuite only on pylint fatar or error

--- tests/run_suite.sh
+++ tests/run_suite.sh
@@ -20,9 +20,11 @@ SCORE=0
 
 pushd .. &> /dev/null
 ANABOT_DIR=`pwd`
-DOGTAIL_DIR=`install_dogtail $ANABOT_DIR/dogtail/dogtail-*.tar.gz`
-PYTHONPATH="$DOGTAIL_DIR/lib/python2.7/site-packages/"
-pylint --init-hook="sys.path.append('$DOGTAIL_DIR/lib/python2.7/site-packages/')" anabot || SCORE=$[SCORE+1]
+#DOGTAIL_DIR=`install_dogtail $ANABOT_DIR/dogtail/dogtail-*.tar.gz`
+# pylint's return code is bit-ORed value of statuses. See pylint manpage.
+# 3 means: 1 - fatal message + 2 - error message
+pylint --disable=import-error anabot; PYLINT_RETCODE=$?
+test $[PYLINT_RETCODE & 3] -ne 0 && SCORE=$[SCORE+1]
 
 rm -rf -- "$DOGTAIL_DIR"
 popd &> /dev/null
