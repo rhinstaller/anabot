e7295163177dca0bfcbb54a7c6312b1159e8fa3f
Pavel Holica <pholica@redhat.com>
2016-03-16 10:07:55 +0100

    rt: Fix execution of post hooks in chroot.

--- anabot/runtime/hooks.py
+++ anabot/runtime/hooks.py
@@ -19,18 +19,20 @@ def _run_hooks(hooks, chroot=None, preexec_fn=None):
         preexec = tmp_preexec
     for hook in hooks:
         exec_path = hook
-        if chroot is not None:
-            new_dir = os.path.join(chroot, 'tmp')
-            shutil.copy(hook, new_dir)
-            exec_path = os.path.join(new_dir, os.path.basename(hook))
-            logger.debug("Copying hook for chroot to: %s", exec_path)
         os.chmod(exec_path, stat.S_IEXEC)
-        logger.debug("Running hook: %s", exec_path)
+        if chroot is not None:
+            new_path = os.path.join(chroot, 'tmp', os.path.basename(hook))
+            shutil.copy(hook, new_path)
+            exec_path = os.path.join('/', 'tmp', os.path.basename(hook))
+            logger.debug("Copying hook for chroot to: %s", new_path)
+            logger.debug("Running hook (in chroot %s): %s", chroot, exec_path)
+        else:
+            logger.debug("Running hook: %s", exec_path)
         p = subprocess.Popen([exec_path], preexec_fn=preexec)
         p.wait()
         if chroot is not None:
-            logger.debug("Removing hook from chroot: %s", exec_path)
-            os.unlink(exec_path)
+            logger.debug("Removing hook from chroot: %s", new_path)
+            os.unlink(new_path)
 
 def run_prehooks():
     _run_hooks(_hooks('pre'))
