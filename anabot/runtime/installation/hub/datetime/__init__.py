cd882334c25635179644962a3aeaef55ae6c7279
Pavel Holica <pholica@redhat.com>
2016-05-31 18:59:22 +0200

    Region and City selection, and some preparations.

--- anabot/runtime/installation/hub/datetime/__init__.py
+++ anabot/runtime/installation/hub/datetime/__init__.py
@@ -7,9 +7,9 @@ from fnmatch import fnmatchcase
 
 from anabot.runtime.decorators import handle_action, handle_check
 from anabot.runtime.default import default_handler, action_result
-from anabot.runtime.functions import get_attr, getnode, getnodes, getnode_scroll, scrollto
+from anabot.runtime.functions import get_attr, getnode, getnode_scroll, getsibling, combo_scroll
 from anabot.runtime.errors import TimeoutError
-from anabot.runtime.translate import tr
+from anabot.runtime.translate import tr, datetime_tr
 
 _local_path = '/installation/hub/datetime'
 handle_act = lambda x: handle_action(_local_path + x)
@@ -22,6 +22,8 @@ def base_handler(element, app_node, local_node):
     datetime.click()
     datetime_panel = getnode(app_node, "panel", tr("DATE & TIME"))
     default_handler(element, app_node, datetime_panel)
+    done_button = getnode(datetime_panel, "push button", tr("_Done", False))
+    done_button.click()
 
 @handle_chck('')
 def base_check(element, app_node, local_node):
@@ -33,26 +35,61 @@ def base_check(element, app_node, local_node):
 
 @handle_act('/region')
 def region_handler(element, app_node, local_node):
-    pass
+    value = get_attr(element, "value")
+    region_name = datetime_tr(value)
+    region_label = getnode(local_node, "label",
+                           tr("_Region:", context="GUI|Date and Time"))
+    region_combo = getsibling(region_label, 1, "combo box")
+    # can't do this better (simple) way
+    region_combo.actions['press'].do()
+    combo_window = getnode(app_node, "window")
+    region_item = getnode(combo_window, "menu item", region_name)
+    region_item.click()
 
 @handle_chck('/region')
 def region_check(element, app_node, local_node):
-    pass
+    value = get_attr(element, "value")
+    # I don't know why, but the region is not translated in name of widget
+    #region_name = datetime_tr(value)
+    region_name = value
+    region_label = getnode(local_node, "label",
+                           tr("_Region:", context="GUI|Date and Time"))
+    region_combo = getsibling(region_label, 1, "combo box")
+    return unicode(region_combo.name) == unicode(region_name)
 
 @handle_act('/city')
 def city_handler(element, app_node, local_node):
-    pass
+    value = get_attr(element, "value")
+    city_name = datetime_tr(value)
+    city_label = getnode(local_node, "label",
+                         tr("_City:", context="GUI|Date and Time"))
+    city_combo = getsibling(city_label, 1, "combo box")
+    # can't do this better (simple) way
+    city_combo.actions['press'].do()
+    combo_window = getnode(app_node, "window")
+    city_item = getnode(combo_window, "menu item", city_name)
+    combo_scroll(city_item)
+    city_item.click()
 
 @handle_chck('/city')
 def city_check(element, app_node, local_node):
-    pass
+    value = get_attr(element, "value")
+    # I don't know why, but the city is not translated in name of widget
+    #city_name = datetime_tr(value)
+    city_name = value
+    city_label = getnode(local_node, "label",
+                         tr("_City:", context="GUI|Date and Time"))
+    city_combo = getsibling(city_label, 1, "combo box")
+    return unicode(city_combo.name) == unicode(city_name)
 
 @handle_act('/ntp')
 def ntp_handler(element, app_node, local_node):
+    action = get_attr(element, "value")
     pass
 
 @handle_chck('/ntp')
 def ntp_check(element, app_node, local_node):
+    action = get_attr(element, "value")
     pass
 
 @handle_act('/ntp_settings')
@@ -65,10 +102,12 @@ def ntp_settings_check(element, app_node, local_node):
 
 @handle_act('/ntp_settings/server')
 def ntp_settings_server_handler(element, app_node, local_node):
+    hostname = get_attr(element, "hostname")
     pass
 
 @handle_chck('/ntp_settings/server')
 def ntp_settings_server_check(element, app_node, local_node):
+    hostname = get_attr(element, "hostname")
     pass
 
 @handle_act('/time')
@@ -81,34 +120,42 @@ def time_check(element, app_node, local_node):
 
 @handle_act('/time/hours')
 def time_hours_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/time/hours')
 def time_hours_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/time/minutes')
 def time_minutes_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/time/minutes')
 def time_minutes_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/time/format')
 def time_format_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/time/format')
 def time_format_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/time/ampm')
 def time_ampm_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/time/ampm')
 def time_ampm_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/date')
@@ -121,24 +168,30 @@ def date_check(element, app_node, local_node):
 
 @handle_act('/date/month')
 def date_month_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/date/month')
 def date_month_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/date/day')
 def date_day_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/date/day')
 def date_day_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_act('/date/year')
 def date_year_handler(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
 
 @handle_chck('/date/year')
 def date_year_check(element, app_node, local_node):
+    value = get_attr(element, "value")
     pass
