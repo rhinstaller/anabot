1be3289607adb566082993da4a76af8c145b3337
Jiri Kortus <jikortus@redhat.com>
2016-06-16 11:49:44 +0200

    Change __str__ output format

--- anabot/runtime/errors.py
+++ anabot/runtime/errors.py
@@ -15,33 +15,27 @@ class TimeoutError(Exception):
         message = []
         if self.message is not None:
             message.append(self.message)
-        if self.data is not None:
-            if type(self.data) == dict:
-                data = filter_dict(self.data)
-                for k, v in data.iteritems():
-                    if k == 'predicates':
-                        data[k] = [predicate.debugName for predicate in v]
-                    elif k == 'predicate':
-                        data[k] = v.debugName
-                    else:
-                        data[k] = str(v)
-                message.append(data)
-                if len(self.args) > 0:
-                    message.append(self.args)
-            else:
-                if len(self.args) == 0:
-                    message.append(str(self.data))
+        message += list(self.args)
+        if self.data is not None and not isinstance(self.data, dict):
+            message.append(self.data)
+        if isinstance(self.data, dict):
+            data = filter_dict(self.data)
+            for k, v in data.iteritems():
+                if k == 'predicates':
+                    data[k] = [predicate.debugName for predicate in v]
+                elif k == 'predicate':
+                    data[k] = v.debugName
                 else:
-                    message.append(tuple([self.data] + list(self.args)))
+                    data[k] = str(v)
+            message.append(data)
 
         if len(message) == 0:
             return ""
         elif len(message) == 1:
-            return message[0]
+            return str(message[0])
         else:
             return str(tuple(message))
 
     def __repr__(self):
         return (self.__class__.__name__ + ": "
                 + str(tuple([self.message, self.data, self.args])))
-
