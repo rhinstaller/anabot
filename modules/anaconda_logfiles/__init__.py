6cc097679a23b9f945ccac1fe2f2bc99b30d2a1a
Pavel Holica <pholica@redhat.com>
2016-07-26 15:24:43 +0200

    Initial attempt to send anaconda logfiles.

---
+++ modules/anaconda_logfiles/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module runs in own deamon thread. It watches periodically changes in
+desired files and sends them using via teres.
+"""
+
+import os, time
+import threading
+import teres
+from teres.bkr_handlers import QUIET_FILE
+
+reporter = teres.Reporter.get_reporter()
+watched_files = [
+    '/tmp/anaconda.log',
+    '/tmp/ifcfg.log',
+    '/tmp/packaging.log',
+    '/tmp/program.log',
+    '/tmp/storage.log',
+    '/tmp/sys.log',
+]
+
+def mainloop():
+    time.sleep(5) # FIXME: should be enough time for beaker handler to show up
+    changes = {k: None for k in watched_files}
+    while True:
+        for path, last_changed in changes.iteritems():
+            if not os.path.exists(path):
+                continue
+            changed = os.stat(path).st_mtime
+            if last_changed is not None and last_changed >= changed:
+                continue
+            # modify time is greater then last registered, send the file
+            reporter.send_file(path, flags={QUIET_FILE:True})
+            reporter.log_debug("Sending updated file: %s" % path)
+            changes[path] = changed
+        time.sleep(5)
+
+watch_thread = threading.Thread(target=mainloop)
+watch_thread.daemon = True
+watch_thread.start()
