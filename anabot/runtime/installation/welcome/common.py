afbbbbdba9a70f6f2d993797cc9f98aa8d8e8559
Peter Kotvan <pkotvan@redhat.com>
2017-02-07 14:16:39 +0100

    Log the seeting of translator in welcome.

--- anabot/runtime/installation/welcome/common.py
+++ anabot/runtime/installation/welcome/common.py
@@ -1,7 +1,12 @@
+import logging
+logger = logging.getLogger('anabot')
+
 from anabot.runtime.decorators import handle_action, handle_check
 from anabot.runtime.functions import getnode, getselected
 from anabot.runtime.translate import set_languages_by_name
 
 def set_language(local_node):
     locales = getnode(local_node, "table", "Locales")
-    set_languages_by_name(getselected(locales)[0].name)
+    language = getselected(locales)[0].name
+    set_languages_by_name(language)
+    logger.info("Setting translator to: %s", language)
