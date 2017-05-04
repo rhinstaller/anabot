0d5d41a15f5077ebd07c9a1f53bfa1d850dabc38
Pavel Holica <pholica@redhat.com>
2017-05-04 13:59:50 +0200

    Updated and fixed locality handling.

--- anabot/runtime/installation/welcome/locality.py
+++ anabot/runtime/installation/welcome/locality.py
@@ -1,5 +1,7 @@
-from anabot.runtime.functions import getnode, get_attr
+from anabot.runtime.errors import TimeoutError
+from anabot.runtime.functions import getnode, getnode_scroll, get_attr
 from anabot.runtime.decorators import handle_action, handle_check
+from anabot.runtime.actionresult import ActionResultPass, ActionResultFail, NotFoundResult
 from .common import set_language
 
 import time
@@ -11,18 +13,23 @@ handle_chck = lambda x: handle_check(_local_path + x)
 @handle_act('')
 def locality_handler(element, app_node, local_node):
     locality = get_attr(element, "value")
-    gui_locality = getnode(local_node, "table cell", ".* (%s)" % locality)
-    gui_locality_first = getnode(local_node, "table cell", ".* (.*)")
-    gui_locality_first.click()
-    time.sleep(1)
-    while not gui_locality.selected:
-        gui_locality_first.parent.keyCombo("Down")
-        time.sleep(1)
+    try:
+        gui_locality = getnode_scroll(local_node, "table cell", ".* (%s)" % locality)
+    except TimeoutError:
+        return NotFoundResult("locality table cell", whose=locality)
+    gui_locality.click()
     set_language(local_node)
+    return ActionResultPass()
 
+LOCALITY_NOT_SELECTED = ActionResultFail("Locality '%s' is not selected.")
 @handle_chck('')
 def locality_check(element, app_node, local_node):
     locality = get_attr(element, "value")
-    gui_locality = getnode(local_node, "table cell",
-                           ".* ({0})".format(locality))
-    return gui_locality.selected
+    try:
+        gui_locality = getnode(local_node, "table cell",
+                               ".* ({0})".format(locality))
+    except TimeoutError:
+        return NotFoundResult("locality table cell", whose=locality)
+    if gui_locality.selected:
+        return ActionResultPass()
+    return LOCALITY_NOT_SELECTED % "test"
