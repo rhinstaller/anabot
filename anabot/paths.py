4d0522136302001422e4f80ff76476ce5e23c094
Pavel Holica <pholica@redhat.com>
2017-10-24 07:35:48 +0200

    Add platform specific paths.
    This is preparation for inclusion of libxml2, since it's not in the
    anaconda installation image :(

--- anabot/paths.py
+++ anabot/paths.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, platform
 anabot_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # modules paths
@@ -6,6 +6,14 @@ teres_path = os.path.join(anabot_root, 'teres')
 libs_path = os.path.join(
     anabot_root,
     'lib/python%(major)s.%(minor)s/site-packages' % {
+        'major': sys.version_info.major,
+        'minor': sys.version_info.minor,
+    }
+)
+libs_arch_path = os.path.join(
+    anabot_root,
+    'lib/%(arch)s/python%(major)s.%(minor)s/site-packages' % {
+        'arch': platform.machine(),
         'major': sys.version_info.major,
         'minor': sys.version_info.minor,
     }
@@ -22,8 +30,9 @@ modules_path = os.path.join(anabot_root, 'modules')
 #   anabot path
 sys.path.append(anabot_root)
 #   additional libs path (we don't want to mess the system, so we use our
-#   site-packages dir)
+#   site-packages dirs)
 sys.path.append(libs_path)
+sys.path.append(libs_arch_path)
 #   add modules path
 sys.path.append(modules_path)
 # utility to take screenshot
