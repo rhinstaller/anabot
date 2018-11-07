dc9a64a698baffefb358d38eb0df482dd42ec5d0
Pavel Holica <pholica@redhat.com>
2018-11-07 08:30:53 +0100

    Don't hardcode beaker hub hostname anywhere.

--- make_updates.sh
+++ make_updates.sh
@@ -5,6 +5,10 @@ TMPDIR=`mktemp -d`
 mkdir $TMPDIR/opt
 cp -r * $TMPDIR/opt
 mv $TMPDIR/opt/etc $TMPDIR
+cat > $TMPDIR/opt/anabot.ini <<EOF
+[DEFAULT]
+var_beaker_hub_hostname=${BEAKER_HUB_HOSTNAME}
+EOF
 
 tar -C $TMPDIR -czf $DESTINATION .
 
