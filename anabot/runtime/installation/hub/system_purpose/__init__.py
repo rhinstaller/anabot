8891218ccf63f16b9129fe49459f6e87baf4464f
Petr Janda <pjanda@redhat.com>
2018-12-06 17:21:29 +0100

    syspurpose Incorporated suggested changes
    using disappeared to check that node is not visible,
    @check_action_result instead of checking action_result directly,
    NotFoundResult class instead of own function

--- anabot/runtime/installation/hub/system_purpose/__init__.py
+++ anabot/runtime/installation/hub/system_purpose/__init__.py
@@ -7,15 +7,11 @@ import fnmatch
 import random
 import six
 
-from anabot.runtime.decorators import handle_action, handle_check
-from anabot.runtime.default import default_handler, action_result
-from anabot.runtime.functions import get_attr, getnode, getnode_scroll, getnodes, getsibling, combo_scroll, type_text, press_key
-from anabot.runtime.errors import TimeoutError
+from anabot.runtime.decorators import handle_action, handle_check, check_action_result
+from anabot.runtime.default import default_handler
+from anabot.runtime.functions import get_attr, getnode, getsibling, disappeared, getnode_scroll
 from anabot.runtime.translate import tr
-from anabot.runtime.actionresult import notfound as notfound_new
-
-def notfound(*args, **kwargs):
-    return (False, notfound_new(*args, **kwargs))
+from anabot.runtime.actionresult import NotFoundResult
 
 _local_path = '/installation/hub/syspurpose'
 handle_act = lambda x: handle_action(_local_path + x)
@@ -28,29 +24,28 @@ def syspurpose_handler(element, app_node, local_node):
     try:
         syspurpose = getnode_scroll(app_node, "spoke selector", syspurpose_label)
     except TimeoutError:
-        return notfound(syspurpose_label, where="main hub")
+        return NotFoundResult(syspurpose_label, where="main hub")
     syspurpose.click()
     try:
         syspurpose_panel = getnode(app_node, "panel", tr("SYSTEM PURPOSE"))
     except TimeoutError:
-        return notfound(tr("SYSTEM PURPOSE") + " SYSTEM PURPOSE spoke")
+        return NotFoundResult(tr("SYSTEM PURPOSE") + " SYSTEM PURPOSE spoke")
     default_handler(element, app_node, syspurpose_panel)
     try:
         done_button = getnode(syspurpose_panel, "push button",
                 tr("_Done", drop_underscore=False,
                 context="GUI|Spoke Navigation"))
     except TimeoutError:
-        return notfound("Done button", where="System Purpose spoke")
+        return NotFoundResult("Done button", where="System Purpose spoke")
     done_button.click()
     return True
 
 
 @handle_chck('')
+@check_action_result
 def syspurpose_check(element, app_node, local_node):
-    if action_result(element)[0] != False:
-        return action_result(element)
     try:
-        getnode(app_node, "panel", tr("SYSTEM PURPOSE"), visible=False)
+        disappeared(app_node, "panel", tr("SYSTEM PURPOSE"))
         return True
     except TimeoutError:
         return (False, "System purpose spoke is still visible.")
@@ -65,39 +60,38 @@ def role_handler(element, app_node, local_node):
     try:
         role_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % role_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % role_name, where="System purpose panel")
     try:
         role_label = getnode(role_panel_label.parent, "label", role_name)
     except TimeoutError:
-        return notfound("label '%s'" % role_name, where="Role selection")
+        return NotFoundResult("label '%s'" % role_name, where="Role selection")
     try:
         role_radio = getsibling(role_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % role_name, where="Role selection")
+        return NotFoundResult("radio button for '%s'" % role_name, where="Role selection")
     if not role_radio.checked:
         role_radio.click()
     return True
 
 
 @handle_chck('/role')
+@check_action_result
 def role_check(element, app_node, local_node):
-    if action_result(element)[0] == False:
-        return action_result(element)
     value = get_attr(element, "value")
     role_name = tr(value)
     panel_name = tr("Role")
     try:
         role_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % panel_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % panel_name, where="System purpose panel")
     try:
         role_label = getnode(role_panel_label.parent, "label", role_name)
     except TimeoutError:
-        return notfound("label '%s'" % role_name, where="Role selection")
+        return NotFoundResult("label '%s'" % role_name, where="Role selection")
     try:
         role_radio = getsibling(role_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % role_name, where="Role selection")
+        return NotFoundResult("radio button for '%s'" % role_name, where="Role selection")
     return role_radio.checked
 
 
@@ -109,39 +103,38 @@ def sla_handler(element, app_node, local_node):
     try:
         sla_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % panel_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % panel_name, where="System purpose panel")
     try:
         sla_label = getnode(sla_panel_label.parent, "label", sla_name)
     except TimeoutError:
-        return notfound("label '%s'" % sla_name, where="SLA selection")
+        return NotFoundResult("label '%s'" % sla_name, where="SLA selection")
     try:
         sla_radio = getsibling(sla_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % sla_name, where="SLA selection")
+        return NotFoundResult("radio button for '%s'" % sla_name, where="SLA selection")
     if not sla_radio.checked:
         sla_radio.click()
     return True
 
 
 @handle_chck('/sla')
+@check_action_result
 def sla_check(element, app_node, local_node):
-    if action_result(element)[0] == False:
-        return action_result(element)
     value = get_attr(element, "value")
     sla_name = tr(value)
     panel_name = tr("Red Hat Service Level Agreement")
     try:
         sla_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % panel_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % panel_name, where="System purpose panel")
     try:
         sla_label = getnode(sla_panel_label.parent, "label", sla_name)
     except TimeoutError:
-        return notfound("label '%s'" % sla_name, where="SLA selection")
+        return NotFoundResult("label '%s'" % sla_name, where="SLA selection")
     try:
         sla_radio = getsibling(sla_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % sla_name, where="SLA selection")
+        return NotFoundResult("radio button for '%s'" % sla_name, where="SLA selection")
     return sla_radio.checked
 
 
@@ -153,37 +146,36 @@ def usage_handler(element, app_node, local_node):
     try:
         usage_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % panel_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % panel_name, where="System purpose panel")
     try:
         usage_label = getnode(usage_panel_label.parent, "label", usage_name)
     except TimeoutError:
-        return notfound("label '%s'" % usage_name, where="SLA selection")
+        return NotFoundResult("label '%s'" % usage_name, where="SLA selection")
     try:
         usage_radio = getsibling(usage_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % usage_name, where="SLA selection")
+        return NotFoundResult("radio button for '%s'" % usage_name, where="SLA selection")
     if not usage_radio.checked:
         usage_radio.click()
     return True
 
 
 @handle_chck('/usage')
+@check_action_result
 def usage_check(element, app_node, local_node):
-    if action_result(element)[0] == False:
-        return action_result(element)
     value = get_attr(element, "value")
     usage_name = tr(value)
     panel_name = tr("Usage")
     try:
         usage_panel_label = getnode(local_node, "label", panel_name)
     except TimeoutError:
-        return notfound("label '%s'" % panel_name, where="System purpose panel")
+        return NotFoundResult("label '%s'" % panel_name, where="System purpose panel")
     try:
         usage_label = getnode(usage_panel_label.parent, "label", usage_name)
     except TimeoutError:
-        return notfound("label '%s'" % usage_name, where="SLA selection")
+        return NotFoundResult("label '%s'" % usage_name, where="SLA selection")
     try:
         usage_radio = getsibling(usage_label, -1, "radio button")
     except TimeoutError:
-        return notfound("radio button for '%s'" % usage_name, where="SLA selection")
+        return NotFoundResult("radio button for '%s'" % usage_name, where="SLA selection")
     return usage_radio.checked
