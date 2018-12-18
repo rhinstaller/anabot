37d162077c70bdee80610032b4ddf987f38026ba
Pavel Holica <pholica@redhat.com>
2018-12-18 14:00:41 +0100

    Don't wait for depsolving during interactive kickstart

--- anabot/runtime/installation/hub/__init__.py
+++ anabot/runtime/installation/hub/__init__.py
@@ -7,6 +7,7 @@ from fnmatch import fnmatchcase
 from time import sleep
 
 from anabot.conditions import is_distro_version_ge
+from anabot.variables import get_variable
 from anabot.runtime.decorators import handle_action, handle_check, check_action_result
 from anabot.runtime.default import default_handler, action_result
 from anabot.runtime.functions import getnode, TimeoutError
@@ -16,9 +17,11 @@ from anabot.runtime.translate import tr
 # submodules
 from . import datetime, keyboard, partitioning, software_selection, oscap_addon, language, system_purpose
 
-@handle_action('/installation/hub')
-def hub_handler(element, app_node, local_node):
-    local_node = getnode(app_node, "panel", tr("INSTALLATION SUMMARY"))
+def _wait_for_depsolve():
+    if is_distro_version_ge('rhel', 8) and get_variable('interactive_kickstart', '0') == '1':
+        # The depsolving doesn't seem to be happening during kickstart
+        # installation on RHEL-8.
+        return
     reporter.log_info("Waiting for package depsolving. Timeout is 10 minutes")
     waitline = ".*DEBUG yum.verbose.YumBase: Depsolve time:.*"
     if is_distro_version_ge('rhel', 8):
@@ -27,6 +30,11 @@ def hub_handler(element, app_node, local_node):
         reporter.log_info("Depsolving finished, nothing should block anaconda main thread now (GTK/ATK) now")
     else:
         reporter.log_info("Depsolving _NOT_ finished, it may happen, that anaconda main thread (GTK/ATK) will be blocked in future and prevent anabot working properly.")
+
+@handle_action('/installation/hub')
+def hub_handler(element, app_node, local_node):
+    local_node = getnode(app_node, "panel", tr("INSTALLATION SUMMARY"))
+    _wait_for_depsolve()
     default_handler(element, app_node, local_node)
 
 @handle_check('/installation/hub')
