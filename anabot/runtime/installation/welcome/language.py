362fd4a0993acad227518af975af228fe7c5ccc8
Pavel Holica <pholica@redhat.com>
2019-07-31 07:19:33 +0200

    Use prefixed handlers in laguage selection.

--- anabot/runtime/installation/welcome/language.py
+++ anabot/runtime/installation/welcome/language.py
@@ -1,10 +1,10 @@
-from anabot.runtime.decorators import handle_action, handle_check
+from anabot.runtime.decorators import make_prefixed_handle_action, make_prefixed_handle_check
 from anabot.runtime.functions import getnode, get_attr
 from .common import set_language
 
 _local_path = '/installation/welcome/language'
-handle_act = lambda x: handle_action(_local_path + x)
-handle_chck = lambda x: handle_check(_local_path + x)
+handle_act = make_prefixed_handle_action(_local_path)
+handle_chck = make_prefixed_handle_check(_local_path)
 
 @handle_act('')
 def language_handler(element, app_node, local_node):
