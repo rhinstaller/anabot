e0ee608c8ac8d3fb65fed01d456555d378b4af4c
Pavel Holica <pholica@redhat.com>
2020-01-07 08:13:26 +0100

    Detect repo configured on cmdline.

--- modules/cmdline_args/__init__.py
+++ modules/cmdline_args/__init__.py
@@ -3,6 +3,7 @@ import shlex
 from anabot.variables import set_variable
 
 # defaults
+set_variable('repo_on_cmdline', '0')
 set_variable('interactive_kickstart', '0')
 
 cmdline = open('/proc/cmdline').read()
@@ -10,6 +11,8 @@ for arg in shlex.split(cmdline):
     try:
         key, value = arg.split('=', 1)
         # kwarg is here
+        if key in ('inst.repo', 'repo'):
+            set_variable('repo_on_cmdline', '1')
         elif key in ('inst.ks', 'ks'):
             set_variable('interactive_kickstart', '1')
     except ValueError:
