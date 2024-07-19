c3295de9a684a2916d35301e9a912e259002b440
Jiri Kortus <jikortus@redhat.com>
2024-07-19 13:27:29 +0200

    [RTT-5714] Create a wrapper for gnome-kiosk
    so that gnome-kiosk is executed with --unsafe-mode,
    which is needed in order to enable introspection API

--- anabot-prepare.sh
+++ anabot-prepare.sh
@@ -3,3 +3,13 @@
 # Deploy non-git (in bundled directory) content in the installer environment
 cp -r /opt/bundled-bin/$(uname -m)/* /
 [ -d /opt/bundled-bin/noarch ] && cp -r /opt/bundled-bin/noarch/* /
+
+# Create a wrapper for gnome-kiosk to run it with introspection enabled
+# (which means with '--unsafe-mode' parameter)
+echo "Creating wrapper script for /bin/gnome-kiosk to run in unsafe mode"
+mv /bin/gnome-kiosk /bin/gnome-kiosk_
+cat > /bin/gnome-kiosk << EOF
+#!/usr/bin/bash
+/bin/gnome-kiosk_ "\$@" --unsafe-mode
+EOF
+chmod +x /bin/gnome-kiosk
