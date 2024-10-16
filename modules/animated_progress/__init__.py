eb64f956bc51605341f556d655a336aa904f1414
Jiri Kortus <jikortus@redhat.com>
2024-10-16 16:48:46 +0200

    [RTT-5714] Temporarily disable screenshot functionality
    until RHEL-64420 is resolved.

--- modules/animated_progress/__init__.py
+++ modules/animated_progress/__init__.py
@@ -8,6 +8,8 @@ reporter = teres.Reporter.get_reporter()
 from anabot.runtime.hooks import register_post_hook
 sys.path.append(os.path.join(os.path.dirname(__file__), 'pngs2apng'))
 import pngs2apng
+from anabot.conditions import is_distro_version_ge
+import logging
 
 screenshot_dir = '/var/run/anabot'
 screenshot_re = re.compile('[0-9]+-screenshot.png')
@@ -16,6 +18,12 @@ target_path = os.path.join(screenshot_dir, target_name)
 
 @register_post_hook(80)
 def make_slideshow_from_screenshots():
+    # Temporarily disabled on RHEL-10 and Fedora 40+ because of broken screenshot
+    # functionality in dogtail - to be removed when RHEL-62420 is fixed.
+    if is_distro_version_ge('rhel', 10) or is_distro_version_ge('fedora', 40):
+        logger = logging.getLogger("anabot")
+        logger.info("No screenshots available with Wayland, slideshow won't be generated.")
+        return
     inputs = []
     for filename in sorted(os.listdir(screenshot_dir)):
         if screenshot_re.match(filename):
