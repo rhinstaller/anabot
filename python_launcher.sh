4deed67555cb8d387cc586b486c15a813c9e976e
Jiri Kortus <jikortus@redhat.com>
2024-10-24 16:17:37 +0200

    [RTT-5714] Move launch delay from service to python_launcher
    The delay in ExecStartPre in anabot.service make Anaconda fail to
    start for some reason on RHEL-10 (more specifically the gnome-kiosk
    launcher script fails). Moving the delay to python_launcher should
    do basically the same job.

--- python_launcher.sh
+++ python_launcher.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# The sleep has been moved from the unit file. It's necessary (at least on <= RHEL-9)
+# to work around issues with dogtail not being able to properly connect to the
+# ATK D-Bus.
+sleep 30
 PYTHON=`which /usr/libexec/platform-python python3 python2 2> /dev/null | head -n 1`
 exec $PYTHON "$@"
