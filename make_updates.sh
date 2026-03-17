9f2b3379b1e689d083b44c5990012ac028ba2544
Jan Stodola <jstodola@redhat.com>
2026-03-17 17:22:20 +0100

    [INSTALLER-4454] Allow anabot to install CA certificate
    Anabot will install CA certificate defined in the CA_CERTIFICATE_URL
    repository secret.

--- make_updates.sh
+++ make_updates.sh
@@ -8,6 +8,7 @@ mv $TMPDIR/opt/etc $TMPDIR
 cat > $TMPDIR/opt/anabot.ini <<EOF
 [DEFAULT]
 var_beaker_hub_hostname=${BEAKER_HUB_HOSTNAME}
+var_ca_certificate_url=${CA_CERTIFICATE_URL}
 EOF
 
 tar -C $TMPDIR -czf $DESTINATION .
